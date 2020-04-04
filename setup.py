0cabeaa3c11888e35aefca92ece876748ca82ba2
SonicRift <shanesutro@gmail.com>
2020-04-04 15:33:47 -0600

    Adding in helper files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vestaboard",
+    version="0.0.1",
+    author="Shane Sutro",
+    author_email="shane@shanesutro.com",
+    description="A Vestaboard Wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SonicRift/Vestaboard.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
