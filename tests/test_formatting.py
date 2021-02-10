1cf14d68c52eb8a3bff8518921528e2c671eab89
Shane Sutro <shanesutro@gmail.com>
2021-02-10 15:55:24 -0700

    Added color padding to converLine method

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -37,6 +37,10 @@ def test_word_conversion_with_invalid_characters_fails():
     with pytest.raises(Exception):
         Formatter().convert('test message^*', byWord=True)
 
+def test_convert_line_fails_if_too_many_characters():
+    with pytest.raises(Exception):
+        Formatter().convertLine('This is too many characters for a line')
+
 def test_convert_line_with_centering():
     assert len(Formatter().convertLine('test message')) == 22, 'Should return a list with 22 elements'
     assert Formatter().convertLine('test message') == [0, 0, 0, 0, 0, 20, 5, 19, 20, 0, 13, 5, 19, 19, 1, 7, 5, 0, 0, 0, 0, 0], 'Should add padding to reach 22 characters'
@@ -52,5 +56,53 @@ def test_convert_line_right_justified():
 def test_valid_characters_should_pass():
     assert Formatter()._isValid('abcdefghijklmnopqrstuvwxyz1234567890 !@#$()-+&=;:"%,./?°') == True
 
+def test_with_character_code_at_beginning_of_string():
+    result = Formatter().convertLine('{23}{1} Test')
+    expected = [0, 0, 0, 0, 0, 0, 0, 0, 23, 1, 0, 20, 5, 19, 20, 0, 0, 0, 0, 0, 0, 0]
+    assert result == expected
+
+def test_with_character_code_at_end_of_string():
+    result = Formatter().convertLine('Test {23}{1}')
+    expected = [0, 0, 0, 0, 0, 0, 0, 0, 20, 5, 19, 20, 0, 23, 1, 0, 0, 0, 0, 0, 0, 0]
+    assert result == expected
+
+def test_with_character_code_in_middle_of_text():
+    result = Formatter().convertLine('Test {23}{1} Test')
+    expected = [0, 0, 0, 0, 0, 20, 5, 19, 20, 0, 23, 1, 0, 20, 5, 19, 20, 0, 0, 0, 0, 0]
+    assert result == expected
+
+def test_with_text_between_character_codes():
+    result = Formatter().convertLine('{48}{3} Test {23}{1}')
+    expected = [0, 0, 0, 0, 0, 0, 48, 3, 0, 20, 5, 19, 20, 0, 23, 1, 0, 0, 0, 0, 0, 0]
+    assert result == expected
+
 def test_invalid_characters_should_fail():
     assert Formatter()._isValid('^*') == False
+
+def test_regex_finds_valid_character_codes():
+    actual = Formatter()._getEmbeddedCharCodes('{24}{1}')
+    expected = ['{24}', '{1}']
+    assert actual == expected
+
+def test_regex_returns_num_of_extra_characters():
+    t1 = Formatter()._numCharacterCodes('{13}{2}')
+    e1 = 5
+    t2 = Formatter()._numCharacterCodes('{23}{25}{25}')
+    e2 = 9
+    t3 = Formatter()._numCharacterCodes('There are no codes')
+    e3 = 0
+    assert t1 == e1
+    assert t2 == e2
+    assert t3 == e3
+
+def test_formatter_accepts_padding_colors():
+    t1 = Formatter().convertLine('red', color='red')
+    e1 = [63, 63, 63, 63, 63, 63, 63, 63, 63, 18, 5, 4, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63]
+    t2 = Formatter().convertLine('orange', color='orange')
+    e2 = [64, 64, 64, 64, 64, 64, 64, 64, 15, 18, 1, 14, 7, 5, 64, 64, 64, 64, 64, 64, 64, 64]
+    t3 = Formatter().convertLine('yellow', color='yellow')
+    e3 = [65, 65, 65, 65, 65, 65, 65, 65, 25, 5, 12, 12, 15, 23, 65, 65, 65, 65, 65, 65, 65, 65]
+
+def test_formatter_fails_invalid_colors():
+    with pytest.raises(KeyError):
+        Formatter().convertLine('error', color='pink')
