3733f54a32799b7fa298f4d89dfd74c65d15246e
Shane Sutro <shane@shanesutro.dev>
2022-07-29 19:05:24 -0600

    Add beginning helper methods
    Add in check for local key and new board parameters to support local
    Add helper method, needs refining still

--- vestaboard/__init__.py
+++ vestaboard/__init__.py
@@ -6,13 +6,16 @@ A lightweight wrapper for Vestaboard
 Board - Class
 Installable - Class
 """
+from multiprocessing.sharedctypes import Value
 import requests
 from vestaboard.formatter import Formatter
 import vestaboard.vbUrls as vbUrls
 import warnings
+import json
+import os
 
 class Board:
-  def __init__(self, installable=False, apiKey=False, apiSecret=False, subscriptionId=False):
+  def __init__(self, installable=False, apiKey=False, apiSecret=False, subscriptionId=False, local=False, localKey: str = '', localIP: str = ''):
     """
     Returns an instance of Board().
 
@@ -24,6 +27,18 @@ class Board:
     """
     if installable and not isinstance(installable, Installable):
       raise ValueError(f'Expected the first argument passed to be an instance of Installable, but instead got {type(installable)}. If you are not trying to pass an Installable, specify the name of the arguments, or pass "None" as the first argument.')
+
+    self.local = local
+
+    if self.local:
+      if len(localKey) == 0 or len(localIP):
+        token = get_local_token()
+        self.localKey = token[0]
+        self.localIP = token[1]
+      else:
+        self.localKey = localKey
+        self.localIP = localIP
+      return
 
     if not installable:  #check for cred file
       if (not apiKey or not apiSecret or not subscriptionId):
@@ -47,6 +62,10 @@ class Board:
       self.subscriptionId = installable.subscriptionId or subscriptionId
 
   def post(self, text):
+    if self.local:
+      self._post_local(text)
+      return
+
     headers = {
         "X-Vestaboard-Api-Key" : self.apiKey,
         "X-Vestaboard-Api-Secret" : self.apiSecret
@@ -102,6 +121,46 @@ class Board:
     finalText = Formatter()._raw(charList)
     requests.post(vbUrls.post.format(self.subscriptionId), headers=headers, json=finalText)
 
+  @staticmethod
+  def enableLocalApi(enablementKey=False, saveToFile=True):
+    if not enablementKey or enablementKey is None:
+      raise ValueError('You must supply an enablement key to enable the local API')
+
+    headers = {
+      "X-Vestaboard-Local-Api-Enablement-Token": enablementKey
+    }
+    try:
+      print(vbUrls.enableLocal)
+      response = requests.post(vbUrls.enableLocal, headers=headers)
+      if not response.ok:
+        print('Looks like that didn\'t work. The Vestaboard returned the following message:\n', response.text)
+      else:
+        parsed = response.json()
+        print('Success! Here\'s your local API token:\n', parsed["apiKey"])
+        if saveToFile:
+          with open('./local.txt', 'w') as local:
+            local.write(parsed["apiKey"])
+            print('Saved to ./local.txt')
+            local.close()
+    except OSError as e:
+      raise ConnectionError("I couldn't connect to your board. Are you on the same network as the board you're trying to connect to?", e)
+
+  def _post_local(self, text):
+    print(self.localIP)
+    print(self.localKey)
+    localHeader = {
+      'X-Vestaboard-Local-Api-Key': self.localKey
+    }
+    textArr = []
+    f = Formatter()
+    for i in range(0, 6):
+      textArr.append(f.convertLine(text))
+    finalText = f._raw(textArr)
+    print(finalText)
+    res = requests.post(vbUrls.postLocal.format(self.localIP), headers=localHeader, data=json.dumps(textArr))
+    print(res.text)
+
+
 class Installable:
   def __init__(self, apiKey=False, apiSecret=False, getSubscription=True, saveCredentials=True):
     """
@@ -150,3 +209,14 @@ def get_creds():
     with open('./credentials.txt', 'r') as cred:
       creds = cred.read().splitlines()
       return creds
+
+def get_local_token():
+  with open('./local.txt', 'r') as local:
+    token = local.read().splitlines()
+    if len(token) == 0:
+        raise ValueError("Either your local token or IP was not passed in, and there's nothing stored.\nEither pass in a value for `localKey` and `localIP`, or store a copy of your local key and IP with the name 'local.txt.' Don't forget to include your board's IP address on line 2.")
+    elif len(token) == 1:
+        raise ValueError("Your local.txt file is missing either the local key or your board's IP address. Ensure your key is on line 1 and your IP is on line 2 and try again.")
+    elif len(token) > 2:
+      raise ValueError("Your local.txt file contains more than your key and IP (or has more than two lines of text). Please remove the extra key or line and try again. Your local.txt file should contain only the local key and board IP address, each on their own line.")
+    return (token[0], token[1])
--- vestaboard/vbUrls.py
+++ vestaboard/vbUrls.py
@@ -1,2 +1,4 @@
 subscription = 'https://platform.vestaboard.com/subscriptions'
 post = "https://platform.vestaboard.com/subscriptions/{}/message"
+enableLocal = "http://{}:7000/local-api/enablement"
+postLocal = "http://{}:7000/local-api/message"
