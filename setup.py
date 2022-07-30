375fb09f9868e33b7c942dfcbc300068f3b929ec
Shane Sutro <shane@shanesutro.dev>
2022-07-30 13:27:13 -0600

    Bump setup version number to Alpha 1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Vestaboard",
-    version="1.1.0",
+    version="1.2.0-a1",
     author="Shane Sutro",
     author_email="shane@shanesutro.com",
     description="A Vestaboard Wrapper",
