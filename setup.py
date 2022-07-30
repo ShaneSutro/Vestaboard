5421721a13a1260f8e4a244c57a2265cd54a99b8
Shane Sutro <shane@shanesutro.dev>
2022-07-30 13:31:44 -0600

    Update alpha format in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Vestaboard",
-    version="1.2.0-a1",
+    version="1.2.0a1",
     author="Shane Sutro",
     author_email="shane@shanesutro.com",
     description="A Vestaboard Wrapper",
